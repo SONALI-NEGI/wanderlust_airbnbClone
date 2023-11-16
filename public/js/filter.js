58bccfbf80dd014c4cbc6a1cc1ea549d89a4fcfa
Sonali Negi <sonalinegi0213@gmail.com>
2023-11-16 21:53:08 +0530

    Edited taxswitch

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,6 +1,31 @@
+
 let taxSwitch = document.getElementById("flexSwitchCheckDefault");
 
 taxSwitch.addEventListener("click", () => {
+  let taxInfo = document.getElementsByClassName("tax-info");
+  let priceInfo = document.getElementsByClassName("price-info");
+  for (info of taxInfo) {
+    if (info.style.display != "inline") {
+      info.style.display = "inline";
+    } else {
+      info.style.display = "none";
+    }
+  }
+
+  for (price of priceInfo) {
+    if (price.style.display != "none") {
+      price.style.display = "none";
+    } else {
+      price.style.display = "inline";
+    }
+  }
+});
+
+
+// taxSwitch for media queries
+let taxSwitch1 = document.getElementById("flexSwitchCheckDefault1");
+
+taxSwitch1.addEventListener("click", () => {
   let taxInfo = document.getElementsByClassName("tax-info");
   let priceInfo = document.getElementsByClassName("price-info");
   for (info of taxInfo) {
